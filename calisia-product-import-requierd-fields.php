9b94fb1667188e2a9a4f9d409faaa7eef2d4e4e8
devunplugged <66313338+devunplugged@users.noreply.github.com>
2021-06-30 15:53:21 +0200

    bug fix
    debugger placed <pre> tags in output

--- calisia-product-import-requierd-fields.php
+++ calisia-product-import-requierd-fields.php
@@ -9,28 +9,27 @@
 define('CALISIA_PRODUCT_IMPORT_REQUIERD_FIELDS_ROOT', __DIR__);
 define('CALISIA_PRODUCT_IMPORT_REQUIERD_FIELDS_URL', plugin_dir_url( __FILE__ ));
 
-
+//require_once CALISIA_PRODUCT_IMPORT_REQUIERD_FIELDS_ROOT . '/src/core/debugger.php';
 
 add_filter( 'woocommerce_product_import_pre_insert_product_object', 'calisia_product_import_requierd_fields::validate', 10, 2 );
 
 class calisia_product_import_requierd_fields{
     public static function validate($object, $data){
-     /*   ob_start();
 
-       // print_r($object);
+        if(!isset($data['name'])){
 
-        print_r($data);
-        $text = ob_get_contents();
-        ob_end_clean();*/
-
-        if(!isset($data['name']))
             throw new Exception( __('No product name provided.', 'calisia-product-import-requierd-fields') );
+        }
 
-        if(!isset($data['regular_price']))
+        if(!isset($data['regular_price'])){
+
             throw new Exception( __('No product price provided.', 'calisia-product-import-requierd-fields') );
+        }
 
-        if(!isset($data['sku']))
+        if(!isset($data['sku'])){
+
             throw new Exception( __('No SKU provided.', 'calisia-product-import-requierd-fields') );
+        }
 
         return $object;
     }
---
+++ src/core/debugger.php
@@ -0,0 +1,28 @@
+<?php
+namespace calisia_product_import_requierd_fields;
+
+class debugger{
+
+    public static function debug($element, $file = true){
+
+        ob_start();
+            print_r($element);
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        if($file){
+            self::to_file($content);
+        }else{
+            echo "<pre>";
+            echo $content;
+            echo "</pre>";
+        }
+    }
+
+   public static function to_file($text){
+        $fp = fopen(CALISIA_PRODUCT_IMPORT_REQUIERD_FIELDS_ROOT . '/debug_log.txt', 'a');//opens file in append mode.
+        fwrite($fp, "$text\n ");
+        fclose($fp);
+    }
+
+}
